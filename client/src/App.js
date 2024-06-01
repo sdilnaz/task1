a8bae767229c3fb150ede18caf15662847d2196f
sdilnaz <dilnaz.sembekova@gmail.com>
2024-06-01 23:23:16 +0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import {BrowserRouter, Routes, Route} from 'react-router-dom'
-// import Home from './pages/Home'
-// import Navbar from './components/Navbar';
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <Navbar setCategory={setCategory} />
-//         <div className='pages'>
-//           <Routes>
-//             <Route path = "/" element = {<Home/>} />
-//           </Routes>
-//         </div>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
